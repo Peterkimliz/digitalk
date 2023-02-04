e99e709c3fff0421a7166452a5e554f827db935a
Freddy <githumbi3fred@gmail.com>
2023-02-04 17:07:28 +0300

    created custom_text_field

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Register extends StatelessWidget {
-   Register({Key? key}) : super(key: key);
+  Register({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Sign Up"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,19 @@
+import 'package:digitalk/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+   CustomTextField({Key? key,required this.controller}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: buttonColor,width: 2)
+        )
+      ),
+    );
+  }
+}
