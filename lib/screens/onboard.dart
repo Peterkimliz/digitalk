af599bf8e4cf6d6f01ead0ac38e3730b96b2b53f
Freddy <githumbi3fred@gmail.com>
2023-02-04 16:50:05 +0300

    added o board screen

--- lib/screens/onboard.dart
+++ lib/screens/onboard.dart
@@ -1,3 +1,6 @@
+import 'package:digitalk/screens/auth/login.dart';
+import 'package:digitalk/screens/auth/register.dart';
+import 'package:digitalk/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class Onboard extends StatelessWidget {
@@ -7,14 +10,26 @@ class Onboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome to \nDigiTalk",
-              style: TextStyle(color: Colors.black, fontSize: 40),
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to \nDigiTalk",
+                style: TextStyle(color: Colors.black, fontSize: 40),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(text: "Log In", onTap: () => Login()),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(text: "Sign Up", onTap: () => Register())
+            ],
+          ),
         ),
       ),
     );
